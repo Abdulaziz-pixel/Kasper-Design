4ebede2812199cb4d69b2c7c245fc0ec9804c8be
Abdulaziz Alshakih <abdulazizmohamed626@gmail.com>
2021-06-16 09:34:02 +0300

    Update the main.js file

--- js/main.js
+++ js/main.js
@@ -1,28 +1,36 @@
-"use strict";
+'use strict';
 // Start ShowModel click
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(" .modal + .overlay");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector(' .modal + .overlay');
 
-const btnShowClick = document.querySelector(".video .text button");
+const btnShowClick = document.querySelector('.video .text button');
 
 console.log(overlay);
 
-btnShowClick.addEventListener("click", function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+btnShowClick.addEventListener('click', function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 });
 // End ShowModel click
 
 // Start CloseModel click
-const CloseModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-const btnCloseModal = document.querySelector(".video .text .modal button");
+const btnCloseModal = document.querySelector('.video .text .modal button');
 
-btnCloseModal.addEventListener("click", CloseModal);
+btnCloseModal.addEventListener('click', closeModal);
 
-overlay.addEventListener("click", CloseModal);
+overlay.addEventListener('click', closeModal);
 
 // End CloseModel click
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
